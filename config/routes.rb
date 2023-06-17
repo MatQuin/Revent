c9d83a6532aae5b11ef9da43f53e20d53ed97da2
Matthieu Quiniou <matthieu.quiniou@hotmail.fr>
2023-06-17 16:18:02 +0200

    seedé

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "dashboard", to: "pages#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :events, only: [:edit, :update, :destroy]
   resources :users do
     resources :events, only: [:new, :create]
   end
