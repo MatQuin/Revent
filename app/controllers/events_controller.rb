bb7f31f456f9bd161e2a1907598bd0a90a3da5c7
aurora muggianu <auroraemu@gmail.com>
2023-06-17 15:31:15 +0200

    create_event new wip

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,37 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def show
+    @event = Event.new
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, status: :see_other
+  end
+
+  # lock an event
+
+private
+
+  def event_params
+    params.require(:event.permit(:address, :price, :start_date, :end_date, :color, :status))
+  end
 end
+
+#il manque une description pour le event
