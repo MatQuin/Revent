9552774c552fcd12cdaa4007c9cf0eddd5d619bb
Matthieu Quiniou <matthieu.quiniou@hotmail.fr>
2023-06-17 15:51:06 +0200

    controller event preskdone

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,26 @@
 class EventsController < ApplicationController
+
+before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+
+def index
+  if params[:query].present?
+    @query = params[:query]
+    @events = Event.where("title ILIKE ?", "%#{params[:query]}%")
+    # Preventing SQL Injection and Database error for
+    # unknown characters
+  else
+    @events = Event.all
+    @markers = @events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
+  end
+end
+
   def new
     @event = Event.new
   end
@@ -20,6 +42,14 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def update
+    if @event.update(event_params)
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @event.destroy
     redirect_to events_path, status: :see_other
@@ -29,8 +59,12 @@ class EventsController < ApplicationController
 
 private
 
+  def even_car
+    @event = Event.find(params[:id])
+  end
+
   def event_params
-    params.require(:event.permit(:address, :price, :start_date, :end_date, :color, :status))
+    params.require(:event.permit(:title, :address, :price, :start, :end, :status))
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_102706) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_17_134801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,11 +30,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_102706) do
     t.date "end"
     t.integer "price"
     t.string "color"
-    t.string "status"
-    t.string "boolean"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.boolean "status"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
