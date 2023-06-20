6402ff0dc27179b547ff58728893aeb590ed1357
Matthieu Quiniou <matthieu.quiniou@hotmail.fr>
2023-06-20 20:42:10 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create!(email:"auroraemu@gmail.com", password:"revent")
+
+Event.create!(address:"68 avenue Parmentier", start: "20/07/2023", end: "21/07/2023", user_id: 1, title: "Soiree", status: true)
