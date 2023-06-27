b374defb64d2c8e8c5a95311cfe2e97f1aa77885
Pierre Tran <pierre.tran@yahoo.fr>
2023-06-27 21:56:10 +0200

    activitynew done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,3 +1,55 @@
 class ActivitiesController < ApplicationController
 
-end
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @activities = Activity.all
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+    @event = Event.find(params[:car_id])
+    @activity.event = @event
+    if @activity.save
+      redirect_to dashboard_path
+    else
+  #    render :new, status: :unprocessable_entity
+    render "event/show", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @activity.update(activity_params)
+      redirect_to activity_path(@activity)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @activity.destroy
+    redirect_to activities_path, status: :see_other
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(:start, :end)
+  end
+
+  end
